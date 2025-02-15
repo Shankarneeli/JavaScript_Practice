8349b03b004e775c7cdf6b0e7410c0bf3ded5302
Sai Shankar <91251505+Shankarneeli@users.noreply.github.com>
2025-02-15 09:54:47 +0530

    promises & Async, Await

--- promise.js
+++ promise.js
@@ -1,41 +1,43 @@
-// let promiseOne = new Promise(function (resolve,reject){
+let promiseOne = new Promise(function (resolve,reject){
+setTimeout(function(){
+    console.log("promiseOne is excuted");
+    resolve(); //Never forget to call Resolve here!
+},1000)    
+});
 
-// setTimeout(function(){
-//     console.log("promiseOne is excuted");
-//     resolve(); //Never forget to call Resolve here!
-    
-// },1000)    
-// });
+promiseOne.then(function(){
+    console.log("& Resolved succesfully!");
+});
 
-// promiseOne.then(function(){
-//     console.log("& Resolved succesfully!");
-    
-// });
-
-// new Promise(function (resolve,reject){
-//     setTimeout(function(){
-//             console.log("promiseTwo is excuted");
-//             resolve(); //Never forget to call Resolve here!
+//2nd Way of Writing PROMISE
+new Promise(function (resolve,reject){
+    setTimeout(function(){
+            console.log("promiseTwo is excuted");
+            resolve(); //Never forget to call Resolve here!
             
-//         },1000)  
-    
-// }).then(function(){
-//     console.log("2nd way of writing Promise..");
-    
-// })
+        },1000)  
+}).then(function(){
+    console.log("2nd way of writing Promise..");  
+})
 
-//Async,Await 
-// async function promisethree() {
-//   try {
 
-//     const response =  await fetch('https://jsonplaceholder.typicode.com/users');
-//     let data = await response.json();
-//     console.log(data);
+// Async,Await 
+async function promisethree() {
+  try {
+    const response =  await fetch('https://jsonplaceholder.typicode.com/users');
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+promisethree();
 
-//   } catch (error) {
-
-//     console.log(error);
-
-//   }
-// }
-// promisethree();
+async function promiseFour(){
+    try{
+        const res = await fetch('https://google.com');
+        let Data = await res.json();
+        console.log(Data);
+    } catch(error){
+        console.log(error);
+}
