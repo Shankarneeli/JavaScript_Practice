f377933765ae1b824bd4bccfa2e093b2df0daa7b
Sai Shankar <91251505+Shankarneeli@users.noreply.github.com>
2025-03-17 19:43:24 +0530

    Update Theoretical & logical Concepts.js

--- Theoretical & logical Concepts.js
+++ Theoretical & logical Concepts.js
@@ -137,3 +137,12 @@ document.title = `(${val}) | Shankar Neeli`;
 
 
 // Learn about getBoundingClientRect() in DOM****
+
+// Function Currying
+function add(a) {
+    return function (b) {
+        if (b) return add(a+b);
+         return a;   
+    }
+}
+console.log(add(5)(2)(5)(5)(5)());
