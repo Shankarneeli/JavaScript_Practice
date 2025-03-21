887a7888db77e01acb3b5ca9069f611f5006d5ab
Sai Shankar <91251505+Shankarneeli@users.noreply.github.com>
2025-03-21 16:11:38 +0530

    Code

---
+++ interview_practice.js
@@ -0,0 +1,30 @@
+// 1. Write a program to remove duplicates from an array ?
+
+// Way1
+// function removeDuplicates(arr){
+//     return [...new Set(arr)];
+// }
+// removeDuplicates([1,2,2,2,1,3,4,5,2,1,7]);
+// // way2
+// function noDuplicates(arr){
+//     return Array.from(new Set(arr));
+// }
+// console.log(noDuplicates([1,2,2,2,3,3,4,1,5,6,7,8]));
+
+// 2. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers. 
+
+// function findEven(arr){
+//     const result = [];
+
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i] % 2 ===0){
+//             result.push(arr[i]);
+//         }
+//     }
+//     return result;
+// }
+// const evenNumbers = [1,3,5,6,2,8,10,22,12,31];
+// console.log(`Even Numbers: ${findEven(evenNumbers)}`);
+
+
+3.
